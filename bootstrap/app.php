3c43603e7ca40647e08b4f0294557a902b76459c
sofia1210 <sofia.crespo.r@gmail.com>
2025-12-13 11:32:40 -0400

    feat: Habilitar CORS para todas las rutas API en la configuración del middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
+        
+        // Habilitar CORS para todas las rutas API
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         /**
