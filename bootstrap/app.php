c9d7a350f56be54eadc2347eec3c2fb896132e5b
sofia1210 <sofia.crespo.r@gmail.com>
2025-12-16 16:14:04 -0400

    feat: Configurar proxies confiables para HTTPS en la aplicación

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withTrustedProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO)
     ->withMiddleware(function (Middleware $middleware): void {
+        // Configurar proxies confiables para HTTPS detrás de load balancer/proxy
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
         // Alias de middlewares de Spatie Permission (roles y permisos)
         // OJO: el namespace correcto es Spatie\Permission\Middleware (sin "s")
         $middleware->alias([
